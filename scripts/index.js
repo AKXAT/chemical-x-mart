5af2e734a16242473a78e6b93188cddc1990593f
Akshat Sharma <akshat.aksharma@gmail.com>
2025-10-25 19:52:59 +0530

    added code have quantity from 1 to 5 shown and added a add to card funtion

--- scripts/index.js
+++ scripts/index.js
@@ -8,6 +8,7 @@ fetch('./backend/products.json')
     const products = data;
     productsHtml = printLoop(products);
     document.querySelector('.js-product-grid').innerHTML = productsHtml;
+    addToCart();
   });
 
 
@@ -22,13 +23,29 @@ const printLoop = function(products) {
       <div class="product-price">$${((element.priceCents)/100).toFixed(2)}</div>
       <div class="rating">
         <img src="${element.rating.image}" alt="${element.rating.stars}">
+      <label for="quantity">Quantity:</label>
+      <select id="quantity" name="quantity" class="js-quantity">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
       </div>
-      <button class="add-to-cart">Add to Cart</button>
+      <button class="add-to-cart js-add-to-cart">
+      Add to Cart
+      </button>
     </div>
         `;
     });
     return generateHtml;
 }
 
+ 
 // Update the DOM 
 
+var addToCart = function() {
+  document.querySelectorAll('.js-add-to-cart').forEach((button) => {button.addEventListener('click',() => {
+    alert("test")
+  } )}) 
+}
